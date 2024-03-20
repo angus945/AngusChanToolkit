91a5b1b2caf3a1ee651d236aa7c37a673bebb748
angus9456@gmail.com <angus9456@gmail.com>
2024-03-20 09:48:45 +0800

    rename database toolkit

--- Database/Core/Database.cs
+++ Database/Core/Database.cs
@@ -1,35 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
-public class Database<T>
+public class Database<T> where T : new()
 {
     IPathLocator pathLocator;
-    IDatabaseService database;
+    ISQLService database;
 
-    public Database(string databaseName, IPathLocator pathLocator, IDatabaseService database)
+    public Database(string databaseName, IPathLocator pathLocator, ISQLService database)
     {
         this.pathLocator = pathLocator;
-        // this.syncer = syncer;
         this.database = database;
 
         string dbPath = pathLocator.GetDatabasePath(databaseName);
         database.Initialize(dbPath);
     }
-
-    // public void ReDownloadDB()
-    // {
-    //     syncer.SyncDatas<T>((T[] datas) =>
-    //     {
-    //         Debug.Log(datas);
-    //         Debug.Log(datas.Length);
-    //         Debug.Log(datas[0].ToString());
-
-    //         RecreateDB();
-    //         InsertAll(datas);
-    //     });
-    // }
     public void RecreateDB()
     {
         database.DropTable<T>();
@@ -38,5 +25,14 @@ public class Database<T>
     public void InsertAll(T[] objs)
     {
         database.InsertAll(objs);
+    }
+    public IEnumerable<T> GetItem(Expression<Func<T, bool>> expression)
+    {
+        return database.GetItems<T>(expression);
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return database.GetItems<T>();
     }
 }
--- Database/implement/SQLiteService.cs
+++ Database/implement/SQLiteService.cs
@@ -1,32 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using SQLite4Unity3d;
 using UnityEngine;
 
-public class SQLiteService : IDatabaseService
+public class SQLiteService : ISQLService
 {
     string databasePath;
     SQLiteConnection connection;
 
 
-    void IDatabaseService.Initialize(string databasePath)
+    void ISQLService.Initialize(string databasePath)
     {
         this.databasePath = databasePath;
 
         connection = new SQLiteConnection(databasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
     }
 
-    void IDatabaseService.InsertAll<T>(T[] objs)
-    {
-        connection.InsertAll(objs);
-    }
-    void IDatabaseService.CreateTable<T>()
+    void ISQLService.CreateTable<T>()
     {
         connection.CreateTable<T>();
     }
-    void IDatabaseService.DropTable<T>()
+    void ISQLService.DropTable<T>()
     {
         connection.DropTable<T>();
     }
+    void ISQLService.InsertAll<T>(T[] objs)
+    {
+        connection.InsertAll(objs);
+    }
 
+    IEnumerable<T> ISQLService.GetItems<T>()
+    {
+        return connection.Table<T>();
+    }
+    IEnumerable<T> ISQLService.GetItems<T>(Expression<Func<T, bool>> expression)
+    {
+        return connection.Table<T>().Where(expression);
+    }
 }
